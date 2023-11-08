bf56c82de2ef3570fc2ecc2654a7716e37d12781
s1F10200328 <s1f102003289@iniad.org>
2023-11-08 09:24:57 +0900

    tab1を選択肢で検索しなおせるようにした

--- lib/screens/tab1.dart
+++ lib/screens/tab1.dart
@@ -8,15 +8,16 @@ class Tab1 extends StatefulWidget {
 }
 
 class _Tab1State extends State<Tab1> {
-  static const String key = "AIzaSyA8OXpQMoeDgbb7nkwX4mDpjeCh4UmQkOQ"; // あなたのAPIキーを使用してください
+  static const String key = "AIzaSyA8OXpQMoeDgbb7nkwX4mDpjeCh4UmQkOQ";
 
   YoutubeAPI youtube = YoutubeAPI(key);
   List<YouTubeVideo> videoResult = [];
 
+  String selectedQuery = "簡単レシピ"; // 初期値を簡単レシピに設定
+
   Future<void> callAPI() async {
-    String query = "簡単レシピ";
     videoResult = await youtube.search(
-      query,
+      selectedQuery,
       order: 'relevance',
       videoDuration: 'any',
       regionCode: 'JP',
@@ -32,27 +33,59 @@ class _Tab1State extends State<Tab1> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: videoResult.length,
-      itemBuilder: (context, index) {
-        return ListTile(
-          leading: Image.network(videoResult[index].thumbnail.small.url ?? ''),
-          title: Text(videoResult[index].title),
-          subtitle: Text(videoResult[index].channelTitle),
-          onTap: () {
-            String? selectedVideoId = videoResult[index].id;
-            if (selectedVideoId != null) {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) =>
-                      RecipeGenerator(videoId: selectedVideoId),
-                ),
-              );
-            }
+    return Column(
+      children: [
+        // 選択肢を表示するDropdownButton
+        DropdownButton<String>(
+          value: selectedQuery,
+          items: const <DropdownMenuItem<String>>[
+            DropdownMenuItem<String>(
+              value: "簡単レシピ",
+              child: Text("簡単レシピ"),
+            ),
+            DropdownMenuItem<String>(
+              value: "和食　レシピ",
+              child: Text("和食"),
+            ),
+            DropdownMenuItem<String>(
+              value: "洋食　レシピ",
+              child: Text("洋食"),
+            ),
+          ],
+          onChanged: (String? newValue) {
+            setState(() {
+              selectedQuery = newValue ?? "";
+              callAPI(); // 選択肢が変更されたらAPIを再呼び出し
+            });
           },
-        );
-      },
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: videoResult.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                leading:
+                Image.network(videoResult[index].thumbnail.small.url ?? ''),
+                title: Text(videoResult[index].title),
+                subtitle: Text(videoResult[index].channelTitle),
+                onTap: () {
+                  String? selectedVideoId = videoResult[index].id;
+                  if (selectedVideoId != null) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => RecipeGenerator(
+                          videoId: selectedVideoId,
+                        ),
+                      ),
+                    );
+                  }
+                },
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
