c21ee0bc4310d92780d4854ea5b8cfe1957d8f17
s1F10210255 <s1f102102559@iniad.org>
2023-11-15 08:04:25 +0900

    ロゴ GIF　追加

--- lib/screens/tab1.dart
+++ lib/screens/tab1.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:youtube_api/youtube_api.dart';
 import 'package:g14/screens/recipegenerator.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:video_player/video_player.dart';
+
+
 
 class Tab1 extends StatefulWidget {
   @override
@@ -8,22 +13,66 @@ class Tab1 extends StatefulWidget {
 }
 
 class _Tab1State extends State<Tab1> {
-  static const String key = "AIzaSyA8OXpQMoeDgbb7nkwX4mDpjeCh4UmQkOQ";
+  static const String key = "AIzaSyAd6OIW60UHOBRO_10VhujI6FujyBQsTB4";
 
-  YoutubeAPI youtube = YoutubeAPI(key);
+  YoutubeAPI youtube = YoutubeAPI(key, maxResults: 20, type: 'video');
+
   List<YouTubeVideo> videoResult = [];
 
   String selectedQuery = "簡単レシピ"; // 初期値を簡単レシピに設定
 
+  bool _isLoading = true; //API呼び出し中trueにしておく
+
   Future<void> callAPI() async {
-    videoResult = await youtube.search(
+    setState(() {
+      _isLoading = true; // API呼び出し開始時にtrueに設定
+    });
+
+    List<YouTubeVideo> videos = await youtube.search(
       selectedQuery,
       order: 'relevance',
       videoDuration: 'any',
       regionCode: 'JP',
     );
-    setState(() {});
+
+    // ここで字幕の有無を確認
+    List<YouTubeVideo> videosWithCaptions = [];
+    for (var video in videos) {
+      if (await checkForCaptions(video.id)) {
+        videosWithCaptions.add(video);
+      }
+    }
+
+    // 字幕があるビデオだけをセット
+    setState(() {
+      videoResult = videosWithCaptions;
+      _isLoading = false;
+    });
   }
+
+  Future<bool> checkForCaptions(String? videoId) async {
+    if (videoId == null) return false;
+
+    final response = await http.post(
+      Uri.parse(
+          'https://asia-northeast1-dotted-crane-403823.cloudfunctions.net/youtube_subtitles'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: json.encode({'video_ids': [videoId]}),
+    );
+
+    if (response.statusCode == 200) {
+      var data = json.decode(response.body);
+      // Use a non-nullable string for the key
+      return data['captions'][videoId] ?? false;
+    } else {
+      return false;
+    }
+  }
+
+  VideoPlayerController? _videoController;
+  bool _isVideoInitialized = false;
 
   @override
   void initState() {
@@ -31,60 +80,82 @@ class _Tab1State extends State<Tab1> {
     callAPI();
   }
 
+
+  @override
+  void dispose() {
+    _videoController?.dispose();
+    super.dispose();
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Stack(
       children: [
-        // 選択肢を表示するDropdownButton
-        DropdownButton<String>(
-          value: selectedQuery,
-          items: const <DropdownMenuItem<String>>[
-            DropdownMenuItem<String>(
-              value: "簡単レシピ",
-              child: Text("簡単レシピ"),
+        Column(
+          children: [
+            DropdownButton<String>(
+              value: selectedQuery,
+              onChanged: (String? newValue) {
+                if (newValue != null) {
+                  setState(() {
+                    selectedQuery = newValue;
+                    _isLoading = true; // ローディング状態をtrueに設定
+                    callAPI(); // 新しいクエリでAPIを再呼び出し
+                  });
+                }
+              },
+              items: <DropdownMenuItem<String>>[
+                DropdownMenuItem<String>(
+                  value: "簡単レシピ",
+                  child: Text("簡単レシピ"),
+                ),
+                DropdownMenuItem<String>(
+                  value: "和食　レシピ",
+                  child: Text("和食"),
+                ),
+                DropdownMenuItem<String>(
+                  value: "洋食　レシピ",
+                  child: Text("洋食"),
+                ),
+              ],
             ),
-            DropdownMenuItem<String>(
-              value: "和食　レシピ",
-              child: Text("和食"),
-            ),
-            DropdownMenuItem<String>(
-              value: "洋食　レシピ",
-              child: Text("洋食"),
+            Expanded(
+              child: ListView.builder(
+                itemCount: videoResult.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: Image.network(
+                      videoResult[index].thumbnail.small.url ?? '',
+                      errorBuilder: (context, error, stackTrace) {
+                        return Icon(Icons.error); // エラー時はエラーアイコンを表示
+                      },
+                    ),
+                    title: Text(videoResult[index].title),
+                    subtitle: Text(videoResult[index].channelTitle),
+                    onTap: () {
+                      String? selectedVideoId = videoResult[index].id;
+                      if (selectedVideoId != null) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                RecipeGenerator(
+                                  videoId: selectedVideoId,
+                                ),
+                          ),
+                        );
+                      }
+                    },
+                  );
+                },
+              ),
             ),
           ],
-          onChanged: (String? newValue) {
-            setState(() {
-              selectedQuery = newValue ?? "";
-              callAPI(); // 選択肢が変更されたらAPIを再呼び出し
-            });
-          },
         ),
-        Expanded(
-          child: ListView.builder(
-            itemCount: videoResult.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                leading:
-                Image.network(videoResult[index].thumbnail.small.url ?? ''),
-                title: Text(videoResult[index].title),
-                subtitle: Text(videoResult[index].channelTitle),
-                onTap: () {
-                  String? selectedVideoId = videoResult[index].id;
-                  if (selectedVideoId != null) {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => RecipeGenerator(
-                          videoId: selectedVideoId,
-                        ),
-                      ),
-                    );
-                  }
-                },
-              );
-            },
-          ),
-        ),
+        _isLoading
+            ? Center(child: Image.asset('assets/gif/road.gif')) // ロード中はGIFを表示
+            : SizedBox.shrink(), // ロード完了後は表示しない
       ],
     );
   }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,6 +11,7 @@ import firebase_core
 import rive_common
 import shared_preferences_foundation
 import url_launcher_macos
+import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -19,4 +20,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
